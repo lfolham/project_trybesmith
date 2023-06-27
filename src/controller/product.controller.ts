fced01c48de5c3f890b6f74b338223053dcb7d29
lfolham <lucasfolha@icloud.com>
2023-06-27 16:47:59 -0300

    setup controller/product.controller

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express';
+import productServices from '../services/product.services';
+
+async function create(req:Request, res:Response) {
+    const { name, price, orderId } = req.body;
+    const response = await productServices.create({
+        id: 0, name, price, orderId });
+
+    res.status(201).json(response.data);
+}
 
 export default {
-
+    create,
 };

50c65d26de6449f7f94ba9ff8c1cc407e4c0895c
lfolham <lucasfolha@icloud.com>
2023-06-27 18:25:32 -0300

    testes- mock - done / missing unit tests

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import App from '../../../src/app'
+
+import productMock from '../../mocks/product.mock';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
-
+    it('CREATE está funcionando corretamente', async function() {
+      //arrange
+      const httpRequestBody = productMock.validCreateProductBody;
+      //act
+      const httpResponse = await chai.request(App).post('/products').send(httpRequestBody);
+      //assert
+      expect(httpResponse.status).to.be.equal(201);
+      expect(httpResponse.body).to.be.deep.equal(productMock.validatedCreatedProductBody)
+      //exemplo do dia 04 do course
+    })
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,17 @@
+const validCreateProductBody = {
+    "name": "Martelo de Thor",
+    "price": "30 peças de ouro",
+    "orderId": 4
+}
+
+const validatedCreatedProductBody = {
+    "id": 6,
+    "name": "Martelo de Thor",
+    "price": "30 peças de ouro",
+    "orderId": 4
+}
+
+export default {
+    validCreateProductBody,
+    validatedCreatedProductBody,
+}

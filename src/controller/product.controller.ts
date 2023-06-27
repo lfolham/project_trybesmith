997c7e459881caf12f73234ac33c59d56cf8437c
lfolham <lucasfolha@icloud.com>
2023-06-27 20:42:00 -0300

    setup - Services

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import productServices from '../services/product.services';
 
+async function list(req:Request, res: Response): Promise<Response> {
+  const response = await productServices.list();
+
+  return res.status(200).json(response.data);
+}
+
 async function create(req:Request, res: Response): Promise<Response> {
   const { name, price, orderId } = req.body;
   const response = await productServices.create({
@@ -10,5 +16,6 @@ async function create(req:Request, res: Response): Promise<Response> {
 }
 
 export default {
+  list,
   create,
 };

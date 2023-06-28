a09ce93237fc9f893deb3706e5ed816cac55f26d
lfolham <lucasfolha@icloud.com>
2023-06-27 21:26:28 -0300

    task 4 - setup router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productsRouter from './routes/product.router';
+import loginRouter from './routes/login.router';
 
 const app = express();
 
 app.use(express.json());
 app.use(productsRouter);
+app.use(loginRouter);
 
 export default app;
